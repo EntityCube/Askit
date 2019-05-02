815c5cc30ab111740b8648d7bd2df7c95bc7cbfb
EntityCuber <entitycuber@localhost.localdomain>
2019-05-02 20:10:06 +0530

    just need to link database

--- answer-page.js
+++ answer-page.js
@@ -9,10 +9,11 @@ Label_Question = document.getElementsByClassName("survey-question-label")[0]
 Label_Question_num = document.getElementById("survey-question-number")
 Input_Answer = document.getElementsByClassName("answer-input")[0]
 Box_Answer = document.getElementById("answer-box")
+Btn_Submit = document.getElementById("answer-submit-button")
 
 // Adding Event listeners
 Btn_Next.addEventListener("click", LoadNextQuestion)
-
+Btn_Submit.addEventListener("click", SubmitAnswers)
 
 //loading first value to question
 Label_Question.innerHTML = data[0]
@@ -20,23 +21,28 @@ Label_Question.innerHTML = data[0]
 
 function LoadNextQuestion() {
     if (data.length != answers.length) {
-    if (Input_Answer.value == false) {
-        alert("please write the answer")
-    } else if (Input_Answer.value.length > 101) {
-        alert("character limit exceeded")
-    } else {
-        console.log(Input_Answer.value)
-        answers.push(Input_Answer)
-        Input_Answer.value = ""
-        console.log(answers.length)
-        Label_Question_num.innerHTML = "Question " + (answers.length + 1)
-        Label_Question.innerHTML = data[answers.length]
+        if (Input_Answer.value == false) {
+            alert("please write the answer")
+        } else if (Input_Answer.value.length > 101) {
+            alert("character limit exceeded")
+        } else {
+            console.log(Input_Answer.value)
+            answers.push(Input_Answer)
+            Input_Answer.value = ""
+            console.log(answers.length)
+            Label_Question_num.innerHTML = "Question " + (answers.length + 1)
+            Label_Question.innerHTML = data[answers.length]
 
-        if (data.length == answers.length) {
-            console.log("completed")
-            Box_Answer.style.display = "block"
+            if (data.length == answers.length) {
+                console.log("completed")
+                Box_Answer.style.display = "block"
+
+            }
 
         }
+    }
+}
 
-    }}
+function SubmitAnswers() {
+    alert("its working")
 }
