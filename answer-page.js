9115c6356f6ccc8ef086ae1b5ceec861c18908c4
EntityCuber <48572004+EntityCuber@users.noreply.github.com>
2019-05-13 15:25:11 +0530

    answer page scripting to popup box

--- answer-page.js
+++ answer-page.js
@@ -5,7 +5,8 @@ Btn_Next = document.getElementById("next-button")
 Label_Question = document.getElementsByClassName("survey-question-label")[0]
 Label_Question_num = document.getElementById("survey-question-number")
 Input_Answer = document.getElementsByClassName("answer-input")[0]
-Box_Answer = document.getElementById("pop-up-container")
+Box_Answer = document.getElementById("answer-box")
+Box_Answer_Background = document.getElementById("pop-up-container")
 Btn_Submit = document.getElementById("answer-submit-button")
 Input_Attended_User_Name = document.getElementById("answering-user-name-input")
 
@@ -29,6 +30,7 @@ function LoadNextQuestion() {
             Label_Question_num.innerHTML = "Question " + (answers.length + 1)
             Label_Question.innerHTML = data[answers.length]
             if (data.length == answers.length) {
+                Box_Answer_Background.style.display = "block"
                 Box_Answer.style.display = "block"
             }
 
