db9579f4dccc4b33e59f3a0c00afcf3202cd67cb
EntityCuber <entitycuber@localhost.localdomain>
2019-05-17 19:45:14 +0530

    bug fix

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ function showKey(code) {
     survey_url = "https://askit.netlify.com/survey.html#" + code
     results_url = "https://askit.netlify.com/results.html#" + code
     Label_Heading.innerHTML = "<a href=" + survey_url + ">" + survey_url + "</a>"
-    Btn_Show_Results.innerHTML = "<a href=" + results_url + ">" + " < button > Show Results < /button> " + "</a > "
+    Btn_Show_Results.innerHTML = "<a href=" + results_url + ">" + " <button> Show Results </button> </a > "
 }
 
 function hideAll() {
