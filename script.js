87262078c92139f3e456805087acc31bcf1bd5df
EntityCuber <entitycuber@localhost.localdomain>
2019-05-18 13:47:17 +0530

    bug fix

--- script.js
+++ script.js
@@ -71,6 +71,8 @@ function CopyToClipboard() {
     /* Copy the text inside the text field */
     document.execCommand("copy");
 
+    /* Alert the copied text */
+    alert("Copied the text: " + copyText.value);
 }
 
 function showKey(code) {
