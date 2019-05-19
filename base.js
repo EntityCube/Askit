04857259d2fa7b62377e7eec8af1fd987832375b
EntityCuber <entitycuber@localhost.localdomain>
2019-05-20 04:06:33 +0530

    commit

--- base.js
+++ base.js
@@ -13,6 +13,18 @@ function getCookie(c_name) {
     return "";
 }
 
+var createCookie = function (name, value, days) {
+    var expires;
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    } else {
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
 if (getCookie('data') != "") {
 
     cookie_Data = getCookie('data')
