080b6be04a6d47d92de43abee9e7e87c9de8befb
EntityCuber <entitycuber@localhost.localdomain>
2019-05-20 00:05:11 +0530

    answer ordered

--- rScript.js
+++ rScript.js
@@ -11,6 +11,9 @@ answerData = []
 CurrentAnswerBlock = ""
 bgColor = " "
 AbgColor = []
+AbgColor2 = []
+
+Users = []
 
 //Range function to stack in order
 
@@ -26,21 +29,23 @@ const range = (start, end) => {
 
 function StackOne(i) {
     Qstart = "<p class='question'>"
-    Astart = "<p class='answer' " + "style='background:" + "red" + "'" + ">"
-    Pclose = " </p> "
+    Astart = "<p class='answer' >"
+    Pclose = " </d>"
     Panswer = answerData[0][i - 1]
     NumberOfAnswers = answerData.length
     PQuestion = QStack.innerHTML + Qstart + i + ". " + Questions[i - 1] + Pclose
     PAnswer = Astart + Panswer + Pclose
 
-    for (k of range(0, NumberOfAnswers)) {
-        Astart = "<p class='answer' " + "style='background:" + AbgColor[k] + "'" + ">"
 
-        PAnswer = Astart + answerData[k][i - 1] + Pclose
+
+    for (k of range(0, NumberOfAnswers)) {
+        Astart = "<div class='answer'>"
+
+        PAnswer = Astart + `<div class='OneAnswerWrapper'><div class='Ublock' style='background:linear-gradient( 45deg ,` + AbgColor[k] + "," + AbgColor2[k] + `); padding: 2px; border-radius:0.3em; padding-left:5px;padding-right:5px'>` + Users[k] + ":" + "</div>" + "<div class='rainbow'>" + answerData[k][i - 1] + "</div></div>" + Pclose
         CurrentAnswerBlock = CurrentAnswerBlock + PAnswer
     }
 
-    QStack.innerHTML = PQuestion + CurrentAnswerBlock
+    QStack.innerHTML = PQuestion  + CurrentAnswerBlock
     CurrentAnswerBlock = ""
 }
 
@@ -88,7 +93,7 @@ function getRandomInteger(min, max) {
 // Get Survey Questions
 const QuestionsRef = firebase.database().ref().child(dbDataLocation).child('questions');
 QuestionsRef.on('value', snap => Questions = snap.val())
-"</p> <p class='answer'>"
+"</p><p class='answer'>"
 QuestionsRef.on('value', function () {
     if (Questions !== null) {
         StartStacking()
@@ -112,15 +117,28 @@ function MensionNames() {
 
     for (property in AttendedUsers) {
 
-        red = getRandomInteger(200, 255)
-        green = getRandomInteger(200, 255)
-        blue = getRandomInteger(200, 255)
-        bgColor = "rgba(" + red + "," + green + "," + blue + ",0.1)"
+        red = getRandomInteger(155, 255)
+        green = getRandomInteger(155, 255)
+        blue = getRandomInteger(155, 255)
+        bgColor = "rgba(" + red + "," + green + "," + blue + ",0.3)"
+
+        red2 = red + 50
+        if (red2 > 255) {
+            red2 -= getRandomInteger(10, 50) + 50
+        }
+        green2 = green
+        blue2 = blue
+        bgColor2 = "rgba(" + red2 + "," + green2 + "," + blue2 + ",0.1)"
+
+        console.log("bgcolor ")
         console.log(bgColor)
-
+        console.log("bgColor2 ")
+        console.log(bgColor2)
         AbgColor[j] = bgColor
+        AbgColor2[j] = bgColor2
 
-        NameMension.innerHTML = NameMension.innerHTML + " <span style='background:" + bgColor + ";border-radius:0.2em;padding:1px'>" + property + "</span>  ,"
+        Users[j] = property
+        NameMension.innerHTML = NameMension.innerHTML + /*" <span style='background:linear-gradient( 45deg ," + bgColor + "," + bgColor2 + ");border-radius:0.2em;padding:1px'>" */ " <span style='padding:1px'>" + property + "</span>  ,"
         const AnswersRef = firebase.database().ref().child(dbDataLocation).child('Results').child(property).child('answers')
         AnswersRef.on('value', snap => answerData[j] = snap.val())
         j++;
