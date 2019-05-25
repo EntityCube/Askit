a897d6f57ecc8b6ecd7c7bb42d88e84c22d32625
EntityCuber <entitycuber@localhost.localdomain>
2019-05-25 14:49:34 +0530

    commit

---
+++ l-script.js
@@ -0,0 +1,32 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCNchpc1VOQO9RRuY_LRvb-eCbzK6Uva7E",
+    authDomain: "askit-2f176.firebaseapp.com",
+    databaseURL: "https://askit-2f176.firebaseio.com",
+    projectId: "askit-2f176",
+    storageBucket: "askit-2f176.appspot.com",
+    messagingSenderId: "653490212793",
+    appId: "1:653490212793:web:6c97202ffc216578"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+googleSignIn = () => {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+    }).catch(function (error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+}
